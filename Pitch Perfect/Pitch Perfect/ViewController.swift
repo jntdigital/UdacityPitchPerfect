41707c6dfe0e193465b6cac400f4ab30b7277a32
Joseph Thompson <j.n.thompson@me.com>
2015-03-05 12:01:46 -0500

    Initial Commit - In lesson 2

---
+++ Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Pitch Perfect
+//
+//  Created by Joseph Thompson on 3/4/15.
+//  Copyright (c) 2015 Joseph Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO: Show Text
+        //TODO: Record voice
+        println("in recordAudio")
+        
+    }
+}
+
